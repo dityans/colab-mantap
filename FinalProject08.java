a37de640559f66ca3018f9e114eeacbf9f395b90
Jujukaizen <juanmadhy425@gmail.com>
2025-10-01 10:20:12 +0700

    updatePrintln

--- FinalProject08.java
+++ FinalProject08.java
@@ -1,5 +1,6 @@
 public class FinalProject08 {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama.");
+        System.out.println("Ini baris kode kedua.");
     }
 }
