7fafc8422ae2ff4c8e112e4f15c29fd2322c8330
dityans <dityan25@gmail.com>
2025-10-01 10:22:37 +0700

    update ke 3

--- FinalProject08.java
+++ FinalProject08.java
@@ -2,5 +2,6 @@ public class FinalProject08 {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama.");
         System.out.println("Ini baris kode kedua.");
+        System.out.println("Ini baris kode ketiga.");
     }
 }
